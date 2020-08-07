3b06b88bcf5bec38c5cd1093aa312a916a784d5d
a.a.girish.deshpande <a.a.girish.deshpande@accenture.com>
2020-08-08 00:52:39 +0530

    refactoring

--- src/reducer.js
+++ src/reducer.js
@@ -3,8 +3,7 @@ export const initialState = {
   item: null,
   playing: false,
   playlists: [],
-  token:
-    "BQDO76-BhalwHMZAe0_JOENXBUj8rPfvhAuwze5mUPEB32-pFNSXiwpM7N7M7Fm5SzTT8OrAYjJDg8njOv-q3_aCf5e9gnBs6mK9qw0G6L9LVcf50o2wKWzcVTWjDmF3UhTb61cijri-C5dgApoUm9Wt_qTiSA2jJX5f8XcbWQ9a-5XY",
+  token: null,
 };
 
 export const reducer = (state, action) => {
