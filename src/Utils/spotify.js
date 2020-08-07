d591d105ce726f2e98d3a4aae090ccec87f3b4ca
a.a.girish.deshpande <a.a.girish.deshpande@accenture.com>
2020-08-07 21:02:09 +0530

    decoding token

--- src/Utils/spotify.js
+++ src/Utils/spotify.js
@@ -2,10 +2,25 @@ export const authEndPoint = "https://accounts.spotify.com/authorize";
 const redirectUri = "http://localhost:3000/";
 const clientId = "5f519c09f07447d0af2e6502b95cbf37";
 
-const scopes = [
+let scopes = [
   "user-read-currently-playing",
-  "user-read-currently-played",
+  "user-read-recently-played",
   "user-read-playback-state",
   "user-top-read",
   "user-modify-playback-state",
 ];
+export const getTokenFromUrl = () => {
+  return window.location.hash
+    .substring(1)
+    .split("&")
+    .reduce((initial, item) => {
+      let parts = item.split("=");
+      console.log("the parts", parts);
+      initial[parts[0]] = decodeURIComponent(parts[1]);
+      return initial;
+    }, {});
+};
+
+export const spotifyUrl = `${authEndPoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
+  "%20"
+)}&response_type=token&show_dialog=true`;
