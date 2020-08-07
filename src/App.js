deba5edc965c0697f57b21bb3403f1a208211276
a.a.girish.deshpande <a.a.girish.deshpande@accenture.com>
2020-08-07 22:01:09 +0530

    Changes  to UI

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import Login from "./Components/Login/Login";
 import { getTokenFromUrl } from "./Utils/spotify";
+import SpotifyWebApi from "spotify-web-api-js";
+const spotify = new SpotifyWebApi();
 function App() {
   const [token, setToken] = useState(null);
   useEffect(() => {
     const hash = getTokenFromUrl();
     window.location.hash = "";
     const _token = hash.access_token;
-    if (token) {
+    if (_token) {
       setToken(_token);
+      spotify.setAccessToken(_token);
+      spotify.getMe().then((user) => {
+        console.log(user);
+      });
     }
-    console.log("the token state", token);
   }, []);
   return (
     <div className="app">{token ? <h1>WE are logged in</h1> : <Login />}</div>
---
+++ src/Components/Player/Player.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import "./Player.css";
+function Player() {
+  return <div></div>;
+}
+
+export default Player;
