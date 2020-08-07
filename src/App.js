f1329f848a51d28874aa5abe42ad7f74900e53fb
a.a.girish.deshpande <a.a.girish.deshpande@accenture.com>
2020-08-07 21:17:54 +0530

    Login checker

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import "./App.css";
 import Login from "./Components/Login/Login";
 import { getTokenFromUrl } from "./Utils/spotify";
 function App() {
+  const [token, setToken] = useState(null);
   useEffect(() => {
     const hash = getTokenFromUrl();
     window.location.hash = "";
-    const token = hash.access_token;
-    console.log("the token", token);
+    const _token = hash.access_token;
+    if (token) {
+      setToken(_token);
+    }
+    console.log("the token state", token);
   }, []);
   return (
-    <div className="app">
-      <Login />
-    </div>
+    <div className="app">{token ? <h1>WE are logged in</h1> : <Login />}</div>
   );
 }
 
