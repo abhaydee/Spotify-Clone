53cbedf9f0b8944b811861c8b351f63dd06d2b4b
a.a.girish.deshpande <a.a.girish.deshpande@accenture.com>
2020-08-08 15:12:53 +0530

    adding player icons

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,5 +1,10 @@
 import React from "react";
 import "./Footer.css";
+import PlayCircleOutlineIcon from "@material-ui/icons/PlayCircleOutline";
+import SkipPreviousIcon from "@material-ui/icons/SkipPrevious";
+import SkipNextIcon from "@material-ui/icons/SkipNext";
+import ShuffleIcon from "@material-ui/icons/Shuffle";
+import RepeatIcon from "@material-ui/icons/Repeat";
 function Footer() {
   return (
     <h2 className="footer">
@@ -7,7 +12,11 @@ function Footer() {
         <p>Album and song details</p>
       </div>
       <div className="footer__center">
-        <p>Player controls</p>
+        <ShuffleIcon className="footer__green" />
+        <SkipPreviousIcon className="footer__icon" />
+        <PlayCircleOutlineIcon fontSize="large" className="footer__icon" />
+        <SkipNextIcon className="footer__icon" />
+        <RepeatIcon className="footer__green" />
       </div>
       <div className="footer__right">
         <p>Volume Controls</p>
