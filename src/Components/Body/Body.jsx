7f4d8a36f73cff11f0ea60bfb3a4710fb61ffbba
a.a.girish.deshpande <a.a.girish.deshpande@accenture.com>
2020-08-08 17:26:09 +0530

    removing errors

--- src/Components/Body/Body.jsx
+++ src/Components/Body/Body.jsx
@@ -5,6 +5,7 @@ import { StateProviderValue } from "../../StateProvider";
 import PlayCircleFilledIcon from "@material-ui/icons/PlayCircleFilled";
 import FavouriteIcon from "@material-ui/icons/Favorite";
 import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
+import SongRow from "../SongRow/SongRow";
 function Body({ spotify }) {
   const [{ discover_weekly }, dispatch] = StateProviderValue();
   return (
